5937283f231896c15d59d7820f9fc449271358a8
pedrovanderlansantana <pedromakegam@gmail.com>
2024-05-01 00:05:27 -0300

    exercicio

--- src/Wrappers.java
+++ src/Wrappers.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Wrappers {
+public class Wrappers {
+
+
+    private int idade = 90;
+    private Integer idade2 = idade;
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public Integer getIdade2() {
+        return idade2;
+
+    }
+
+  public Wrappers(){
+
+      System.out.println("Idade: " + getIdade());
+
+  }
+
 }
